70521510480a2387db632f484938a94157875c7f
nicolospuri <nicky.spuri@gmail.com>
2026-04-13 17:09:19 +0200

    setup

---
+++ G48HW1.py
@@ -0,0 +1,54 @@
+from pyspark import SparkContext, SparkConf
+import sys
+import os
+import random as rand
+
+# def FairFFT(ka, kb, Ua, Ub):
+
+
+
+# def MRFairFFT():
+
+
+
+
+
+def main():
+    # CHECKING NUMBER OF CMD LINE PARAMTERS
+    assert len(sys.argv) == 4, "Usage: G48HW1 <data_path> <Ka> <Kb> <L>"
+
+    # SPARK SETUP
+    conf = SparkConf().setAppName('G48HW1')
+    sc = SparkContext(conf=conf)
+
+    # INPUT READING
+
+    # 1. Read number of partitions
+    L = sys.argv[3]
+    assert L.isdigit() and int(L) > 0, "L is not an integer"
+
+    # 3. Read number of centers
+    Ka = sys.argv[2]
+    assert Ka.isdigit() and int(Ka) >= 0, "Ka must be an integer"
+    Ka = int(Ka)
+
+    Kb = sys.argv[2]
+    assert Kb.isdigit() and int(Kb) >= 0, "Ka must be an integer"
+    Kb = int(Kb)
+
+    # 2. Read input file and subdivide it into K random partitions
+    data_path = sys.argv[0]
+    assert os.path.isfile(data_path), "File or folder not found"
+
+    print('File path: ' + data_path + ' Ka: ' + str(Ka) + ' Kb: ' + str(Kb) + " L: " + str(L))
+
+    inputPoints = sc.textFile(data_path).repartition(numPartitions=L).cache()
+
+    # SETTING GLOBAL VARIABLES
+    numPoints = inputPoints.collect().count()
+    print("N = ", numPoints)
+
+    for point in inputPoints.collect():
+
+
+    # Call to MapReduce
