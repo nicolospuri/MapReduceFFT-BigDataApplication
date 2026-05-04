78e6a43d1bc72d4bed3248b9ae866153ed20004e
nicolospuri <nicky.spuri@gmail.com>
2026-05-04 18:12:33 +0200

    minor changes

--- G48HW1.py
+++ G48HW1.py
@@ -14,9 +14,11 @@ def check_arguments(args):
     if len(sys.argv) != 5:
         raise ValueError("Usage: G48HW1 <data_path> <Ka> <Kb> <L>")
 
+    '''
     data_path = sys.argv[1]
     if not os.path.isfile(data_path):
         raise ValueError("File not found")
+    '''
 
     try:
         ka = int(sys.argv[2])
